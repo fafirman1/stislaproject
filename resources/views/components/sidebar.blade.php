a81d7616399c4289288250e755e2a32bd197f68e
fafirman1 <fajarfirmansyah101@gmail.com>
2024-02-29 11:46:37 +0700

    title sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">POS WEB</a>
+            <a href="index.html">Moko Wardah Parfum</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
             <a href="index.html">MWP</a>
