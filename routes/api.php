4a31b660e433f80735b1e20212596be49b2637f7
fafirman1 <fajarfirmansyah101@gmail.com>
2024-03-01 15:43:42 +0700

    Api Logout

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use GuzzleHttp\Middleware;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,8 +19,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 // post login
 Route::post('login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+
+// post logout
+Route::post('logout', [\App\Http\Controllers\Api\AuthController::class,'logout'])->middleware('auth:sanctum');
+
+//Route::post('logout', [\App\Http\Controllers\Api\AuthController::class, 'logout'])->middleware('auth:sanctum');
 
 // api resource product
 Route::apiResource('products', \App\Http\Controllers\Api\ProductController::class)->middleware('auth:sanctum');
