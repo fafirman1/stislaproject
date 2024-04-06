a5af1eb15de765fcc41653a5987ff00a274de436
fafirman1 <fajarfirmansyah101@gmail.com>
2024-04-06 16:03:20 +0700

    api Product app

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -26,7 +26,36 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name'=>'required|min:3',
+            'price'=>'required|integer',
+            'stock'=>'required|integer',
+            'category'=>'required|in:Fresh,Sweet',
+            'image'=>'required|image|mimes:png,jpg,jpeg'
+        ]);
+
+        $filename = time() . '.' . $request->image->extension();
+        $request->image->storeAs('public/products', $filename);
+        $product = \App\Models\Product::create([
+            'name'=>$request->name,
+            'price'=>(int) $request->price,
+            'stock'=>(int) $request->stock,
+            'category'=>$request->category,
+            'image'=>$filename
+        ]);
+
+        if ($product){
+            return response()->json([
+                'success'=>true,
+                'message'=>'Product Created',
+                'data'=>$product
+            ], 201);
+        } else{
+            return response()->json([
+                'success'=>false,
+                'message'=>'Product failed to create',
+            ], 409);
+        }
     }
 
     /**
