0cdd0bc4db4246217e4d19b9c7b697d0e4991261
Fajar Firmansyah <fajarfirmansyah101@gmail.com>
2024-06-02 19:09:01 +0700

    paginate

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -9,7 +9,7 @@ class OrderController extends Controller
     //index
     public function index()
     {
-        $orders = \App\Models\order::with('kasir')->get();
+        $orders = \App\Models\order::with('kasir')->orderBy('created_at','desc')->paginate(10);
         return view('pages.orders.index', compact('orders'));
     }
 
